427c9dea706545548e41fd4a8eb6018b10e13594
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-07-02 21:39:22 +0200

    Added permission check to facets

--- src/app/CHAOS.Portal.Indexing.Extension/Index.cs
+++ src/app/CHAOS.Portal.Indexing.Extension/Index.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using CHAOS.Index;
+using CHAOS.MCM.Module.Rights;
 using CHAOS.Portal.Core;
 using CHAOS.Portal.Core.Extension;
 using CHAOS.Portal.DTO.Standard;
@@ -7,12 +8,29 @@ using CHAOS.Portal.Indexing.Extension.DTO;
 
 namespace CHAOS.Portal.Indexing.Extension
 {
+	/// <summary>
+	/// This module depends on CHAOS.MCM.Module.FolderModule
+	/// </summary>
     public class Index : AExtension
     {
         #region Search
 
-        public void Search( ICallContext callContext, IQuery query )
+		/// <summary>
+		/// Will search the index using the specified query.
+		/// </summary>
+		/// <param name="callContext"></param>
+		/// <param name="query"></param>
+        public void Search( ICallContext callContext, IQuery query, UUID accessPointGUID )
         {
+			if( accessPointGUID != null )
+                query.Query = string.Format( "({0})+AND+(PubStart:[*+TO+NOW]+AND+PubEnd:[NOW+TO+*])", query.Query );
+            else
+            {
+				var folders = callContext.PortalApplication.GetModule<MCM.Module.FolderModule>().Get( callContext, null, null, null, (uint) FolderPermissions.Read );
+
+                query.Query = string.Format( "({0})+AND+({1})", query.Query, string.Join( "+OR+", folders.Select( folder => string.Format( "FolderTree:{0}", folder.ID ) ) ) );
+            }
+
             var response = callContext.IndexManager.GetIndex( "CHAOS.MCM.Module.AMCMModule" ).Get<UUIDResult>( query );
 
             callContext.PortalResponse.PortalResult.GetModule( "Portal" ).AddResult( new IndexResponse( response ) );
