1f31612ef7c36fb75b1f91871438c58afe8b2000
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-07-16 17:48:03 +0200

    Replaced Build batch file with a NAnt build script

--- build.bat
+++ build.bat
@@ -1,12 +1,14 @@
 @echo off
 
 rem Needed to update variable in loop
-setlocal enabledelayedexpansion
+rem setlocal enabledelayedexpansion
 
-for %%i in (bin\IndexExtension\*.dll) do (set files=!files!%%~i )
+rem for %%i in (bin\IndexExtension\*.dll) do (set files=!files!%%~i )
 
-echo Merging IndexExtension
+rem echo Merging IndexExtension
 
-tools\ILMerge\ILMerge.exe /out:build\CHAOS.Portal.IndexExtension.dll /lib:C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /lib:lib\ %files%
+rem tools\ILMerge\ILMerge.exe /out:build\CHAOS.Portal.IndexExtension.dll /lib:C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /lib:lib\ %files%
 
-echo Done
+rem echo Done
+
+tools\nant-0.92\bin\nant.exe -buildfile:build\CHAOS.Portal.Indexing.build
